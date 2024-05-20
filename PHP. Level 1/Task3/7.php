de3f9b2aee11d7049164908a38bb5c275a8df7a8
Mike Onegov <119760634+marabonly@users.noreply.github.com>
2024-05-20 19:07:42 +0500

    Done task3

---
+++ PHP. Level 1/Task3/7.php
@@ -0,0 +1,22 @@
+﻿<html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+</head>
+
+<body>
+    <?php
+
+    /*
+7. *Вывести с помощью цикла for числа от 0 до 9, НЕ используя тело цикла. То есть выглядеть должно так:
+for (…){ // здесь пусто}
+*/
+
+    for ($i = 0; $i <= 9; print($i++)) {
+    }
+
+    ?>
+
+</body>
+
+</html>
