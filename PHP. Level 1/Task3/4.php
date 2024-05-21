7f511ea43d949fcb735df0fbe695d24b840ba089
Mike Onegov <119760634+marabonly@users.noreply.github.com>
2024-05-21 12:07:01 +0500

    Done task3

---
+++ PHP. Level 1/Task3/4.php
@@ -0,0 +1,84 @@
+﻿<html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+</head>
+
+<body>
+    <?php
+
+    /*
+4. Объявить массив, индексами которого являются буквы русского языка,
+а значениями – соответствующие латинские буквосочетания (‘а’=> ’a’, ‘б’ => ‘b’, ‘в’ => ‘v’, ‘г’ => ‘g’, …, ‘э’ => ‘e’, ‘ю’ => ‘yu’, ‘я’ => ‘ya’).
+Написать функцию транслитерации строк.
+*/
+
+    function transliterate($string)
+    {
+        $array = [
+            'а' => 'a',
+            'б' => 'b',
+            'в' => 'v',
+            'г' => 'g',
+            'д' => 'd',
+            'е' => 'e',
+            'ё' => 'yo',
+            'ж' => 'zh',
+            'з' => 'z',
+            'и' => 'i',
+            'й' => 'y',
+            'к' => 'k',
+            'л' => 'l',
+            'м' => 'm',
+            'н' => 'n',
+            'о' => 'o',
+            'п' => 'p',
+            'р' => 'r',
+            'с' => 's',
+            'т' => 't',
+            'у' => 'u',
+            'ф' => 'f',
+            'х' => 'kh',
+            'ц' => 'ts',
+            'ч' => 'ch',
+            'ш' => 'sh',
+            'щ' => 'shch',
+            'ъ' => '\'',
+            'ы' => 'y',
+            'ь' => '\'',
+            'э' => 'e',
+            'ю' => 'yu',
+            'я' => 'ya'
+        ];
+
+        $output = '';
+
+        for ($i = 0; $i < mb_strlen($string); $i++) {
+            $input_char = mb_substr($string, $i, 1);
+            $lowercase_char = mb_strtolower($input_char);
+
+            if ($lowercase_char === $input_char)
+                $isInputCapitalLetter = false;
+            else
+                $isInputCapitalLetter = true;
+
+            if (array_key_exists($lowercase_char, $array)) {
+                $result_char = $array[$lowercase_char];
+                if ($isInputCapitalLetter === true) $result_char = strtoupper($result_char);
+                $output .= $result_char;
+            } else {
+                $output .= $input_char;
+            }
+        }
+
+        return $output;
+    }
+
+    $input = 'GeekBrains — российская компания в сфере онлайн-образования, основанная в 2010 году. Образовательная платформа GeekBrains предлагает курсы по информационным технологиям, программированию, аналитике, тестированию, маркетингу, управлению и дизайну. С 2016 года входит в состав VK.';
+    echo '<h2>Исходный текст:</h2>' . $input . '<br>';
+    echo '<h2>Транслитерация:</h2>' . transliterate($input);
+    ?>
+
+</body>
+
+</html>
---
+++ PHP. Level 1/Task3/6.php
@@ -0,0 +1,91 @@
+<?php
+/*
+6. В имеющемся шаблоне сайта заменить статичное меню (ul - li) на генерируемое через PHP.
+Необходимо представить пункты меню как элементы массива и вывести их циклом.
+Подумать, как можно реализовать меню с вложенными подменю? Попробовать его реализовать.
+*/
+$title = "Заголовок";
+$h1 = "minimalistica";
+$year = 2024;
+
+$menu = [
+	'home',
+	'archive' => [
+		'2022',
+		'2023',
+		'2024'
+	],
+	'contact'
+];
+
+function addMenu($menu, $name)
+{
+	$html = '<ul id="' . $name . '">';
+	foreach ($menu as $key => $menu_element) {
+		if (is_string($key)) {
+			$html .= '<li><a href="#">' . $key . '</a>';
+			$html .= addMenu($menu_element, $key);
+			$html .= '</li>';
+		} else {
+			$html .= '<li><a href="#">' . $menu_element . '</a></li>';
+		}
+	}
+	$html .= '</ul>';
+
+	return $html;
+}
+
+$menu_html = addMenu($menu, 'menu');
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="author" content="Luka Cvrk (www.solucija.com)" />
+	<link rel="stylesheet" href="css/main.css" type="text/css" />
+	<title><?= $title; ?></title>
+</head>
+
+<body>
+	<div id="content">
+		<h1><?= $h1; ?></h1>
+
+		<?= $menu_html; ?>
+
+		<div class="post">
+			<div class="details">
+				<h2><a href="#">Nunc commodo euismod massa quis vestibulum</a></h2>
+				<p class="info">posted 3 hours ago in <a href="#">general</a></p>
+
+			</div>
+			<div class="body">
+				<p>Nunc eget nunc libero. Nunc commodo euismod massa quis vestibulum. Proin mi nibh, dignissim a pellentesque at, ultricies sit amet sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel lorem eu libero laoreet facilisis. Aenean placerat, ligula quis placerat iaculis, mi magna luctus nibh, adipiscing pretium erat neque vitae augue. Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at ipsum.</p>
+			</div>
+			<div class="x"></div>
+		</div>
+
+		<div class="col">
+			<h3><a href="#">Ut enim risus rhoncus</a></h3>
+			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+		<div class="col">
+			<h3><a href="#">Maecenas iaculis leo</a></h3>
+			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+		<div class="col last">
+			<h3><a href="#">Quisque consectetur odio</a></h3>
+			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+
+		<div id="footer">
+			<p>Copyright <?= $year; ?> &copy; <em>minimalistica</em> &middot; Design: Luka Cvrk, <a href="http://www.solucija.com/" title="Free CSS Templates">Solucija</a></p>
+		</div>
+	</div>
+</body>
+
+</html>
